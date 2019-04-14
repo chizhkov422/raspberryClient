33e702045379625d18ced99c785534721091d245
Alexandr Chizhkov <chizhkov422@gmail.com>
2019-04-14 17:27:44 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const io = require('socket.io-client');
+
+const connectUrl = process.argv[2] === 'PROD' ? 'https://raspberry-web-app-server.herokuapp.com' : 'http://localhost:3000';
+console.log(connectUrl)
+const socket = io.connect(connectUrl, { reconnect: true });
+
+// Add a connect listener
+socket.on('connect', () => {
+  console.log('Connected!');
+});
+
+socket.on('checkbox state', (state) => {
+  console.log('State: ', state);
+});
