e652d8ff13ebbb173df2332ff87eb07e72b9ac66
Alexandr Chizhkov <chizhkov422@gmail.com>
2019-05-15 12:27:34 +0300

    Comment out logic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client');
 const Gpio = require('onoff').Gpio;
-const HEATING_ELEMENT = new Gpio(4, 'out');
-// const TEMPERATURE_SENSOR = new Gpio(17, 'in');
+// const HEATING_ELEMENT = new Gpio(4, 'out');
+const TEMPERATURE_SENSOR = new Gpio(17, 'in');
 
 const connectUrl = process.argv[2] === 'PROD' ? 'https://remote-control-iot-server.herokuapp.com/' : 'http://localhost:3000';
 const socket = io.connect(connectUrl, { reconnect: true });
@@ -11,57 +11,56 @@ socket.on('connect', () => {
   console.log('Connected!');
 });
 
-let mode;
-let minValue;
-let maxValue;
-let manualValue;
+// let mode;
+// let minValue;
+// let maxValue;
+// let manualValue;
 
-socket.on('temperatureStateRaspberry', (state) => {
-  mode = state.mode;
-
-  switch (mode) {
-    case 'auto': {
-      minValue = state.minValue;
-      maxValue = state.maxValue;
-
-      HEATING_ELEMENT.writeSync(0);
-      break;
-    }
-    case 'manual': {
-      manualValue = state.manualValue;
-      HEATING_ELEMENT.writeSync(1);
-      break;
-    }
-  }
-});
-
-// TEMPERATURE_SENSOR.watch((err, currentValue) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
+// socket.on('temperatureStateRaspberry', (state) => {
+//   mode = state.mode;
 
 //   switch (mode) {
 //     case 'auto': {
-//       if (currentValue > maxValue) {
-//         HEATING_ELEMENT.writeSync(0);
-//       }
-//       if (currentValue < minValue) {
-//         HEATING_ELEMENT.writeSync(1);
-//       }
+//       minValue = state.minValue;
+//       maxValue = state.maxValue;
 //       break;
 //     }
 //     case 'manual': {
-//       if (currentValue > manualValue) {
-//         HEATING_ELEMENT.writeSync(0);
-//       }
-//       if (currentValue < manualValue) {
-//         HEATING_ELEMENT.writeSync(1);
-//       }
+//       manualValue = state.manualValue;
 //       break;
 //     }
 //   }
+// });
 
-//   socket.emit('temperatureSensorValue', currentValue);
+TEMPERATURE_SENSOR.watch((err, currentValue) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-// })
+  console.log(currentValue)
+
+  // switch (mode) {
+  //   case 'auto': {
+  //     if (currentValue > maxValue) {
+  //       HEATING_ELEMENT.writeSync(0);
+  //     }
+  //     if (currentValue < minValue) {
+  //       HEATING_ELEMENT.writeSync(1);
+  //     }
+  //     break;
+  //   }
+  //   case 'manual': {
+  //     if (currentValue > manualValue) {
+  //       HEATING_ELEMENT.writeSync(0);
+  //     }
+  //     if (currentValue < manualValue) {
+  //       HEATING_ELEMENT.writeSync(1);
+  //     }
+  //     break;
+  //   }
+  // }
+
+  socket.emit('temperatureSensorValue', currentValue);
+
+})
