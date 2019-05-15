d7227119472377cf2783625658d8b8c238440ad6
Alexandr Chizhkov <chizhkov422@gmail.com>
2019-05-15 12:24:45 +0300

    Comment in logic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client');
 const Gpio = require('onoff').Gpio;
 const HEATING_ELEMENT = new Gpio(4, 'out');
-const TEMPERATURE_SENSOR = new Gpio(17, 'in');
+// const TEMPERATURE_SENSOR = new Gpio(17, 'in');
 
 const connectUrl = process.argv[2] === 'PROD' ? 'https://remote-control-iot-server.herokuapp.com/' : 'http://localhost:3000';
 const socket = io.connect(connectUrl, { reconnect: true });
@@ -23,42 +23,45 @@ socket.on('temperatureStateRaspberry', (state) => {
     case 'auto': {
       minValue = state.minValue;
       maxValue = state.maxValue;
+
+      HEATING_ELEMENT.writeSync(0);
       break;
     }
     case 'manual': {
       manualValue = state.manualValue;
+      HEATING_ELEMENT.writeSync(1);
       break;
     }
   }
 });
 
-TEMPERATURE_SENSOR.watch((err, currentValue) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+// TEMPERATURE_SENSOR.watch((err, currentValue) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
 
-  switch (mode) {
-    case 'auto': {
-      if (currentValue > maxValue) {
-        HEATING_ELEMENT.writeSync(0);
-      }
-      if (currentValue < minValue) {
-        HEATING_ELEMENT.writeSync(1);
-      }
-      break;
-    }
-    case 'manual': {
-      if (currentValue > manualValue) {
-        HEATING_ELEMENT.writeSync(0);
-      }
-      if (currentValue < manualValue) {
-        HEATING_ELEMENT.writeSync(1);
-      }
-      break;
-    }
-  }
+//   switch (mode) {
+//     case 'auto': {
+//       if (currentValue > maxValue) {
+//         HEATING_ELEMENT.writeSync(0);
+//       }
+//       if (currentValue < minValue) {
+//         HEATING_ELEMENT.writeSync(1);
+//       }
+//       break;
+//     }
+//     case 'manual': {
+//       if (currentValue > manualValue) {
+//         HEATING_ELEMENT.writeSync(0);
+//       }
+//       if (currentValue < manualValue) {
+//         HEATING_ELEMENT.writeSync(1);
+//       }
+//       break;
+//     }
+//   }
 
-  socket.emit('temperatureSensorValue', currentValue);
+//   socket.emit('temperatureSensorValue', currentValue);
 
-})
+// })
